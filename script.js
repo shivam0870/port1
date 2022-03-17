eea1d0bcac552c2e3b021737869115c817e8c51a
shivam0870 <shivamkrsingh4688@gmail.com>
2022-03-18 02:06:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// typing annimation 
+var typed = new Typed(".typing",{
+    strings:["Web Designer" , "Web developer", "Competetive Coder", "Quick Learner"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
